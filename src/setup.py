c1b6859d693a6d7d0f80cbf474b496fa9934f183
tpaviot <tpaviot@gmail.com>
2020-11-25 04:47:54 +0100

    Bumped version to 2.3.34 to prevent from using latest upstream release

--- src/setup.py
+++ src/setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyschedule',
-    version='0.2.33',
+    version='0.2.34',
     description='A python package to formulate and solve resource-constrained scheduling problems',
-    url='https://github.com/timnon/pyschedule',
+    url='https://github.com/tpaviot/pyschedule',
     author='Tim Nonner',
     author_email='tim@nonner.de',
     license='Apache 2.0',
     packages=find_packages(),
-    include_package_data=True,
-    install_requires=['pulp']
+    install_requires=['pulp'],
+    include_package_data=True
 )
