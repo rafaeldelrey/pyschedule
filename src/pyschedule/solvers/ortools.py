ea75a7c6a26806414f745412d7890e00238a45e6
tpaviot <tpaviot@gmail.com>
2020-12-15 11:46:21 +0100

    Clean up, more doc and explicit variable names, fixes

--- src/pyschedule/solvers/ortools.py
+++ src/pyschedule/solvers/ortools.py
@@ -21,7 +21,7 @@ import collections
 import copy
 
 
-def solve(scenario,time_limit=None,copy_scenario=False,msg=0) :
+def solve(scenario,time_limit=None,copy_scenario=False,msg=False) :
     """ Integration of the ortools scheduling solver """
     try:
         from ortools.constraint_solver import pywrapcp
@@ -32,15 +32,36 @@ def solve(scenario,time_limit=None,copy_scenario=False,msg=0) :
     if copy_scenario :
         S = copy.deepcopy(scenario)
 
+    # create a Solver instance
+    # see documentation at
+    # http://google.github.io/or-tools/python/ortools/constraint_solver/pywrapcp.html#pywrapcp.Solver
     ort_solver = pywrapcp.Solver(S.name)
 
     # tasks
+    # Initialize a dictionnary that maps task to FixedDurationIntervalVar
     task_to_interval = collections.OrderedDict()
-    resource_to_intervals = resource_to_intervals = { R : list() for R in S.resources() }
+
+    # Initialize a directory that maps eahc resource
+    # to a list of FixedDurationIntervalVar
+    # By default this list is empty, it will be appended later in the algorithm
+    resource_to_intervals = {R : [] for R in S.resources()}
     resource_task_to_interval = collections.OrderedDict()
-    for T in S.tasks() :
-        I = ort_solver.FixedDurationIntervalVar(0,S.horizon-T.length,T.length,False,T.name)
-        task_to_interval[T] = I
+
+    # Map each Task to a FixedDurationIntervalVar
+    # Documentation at
+    # http://google.github.io/or-tools/python/ortools/constraint_solver/pywrapcp.html#pywrapcp.Solver.FixedDurationIntervalVar
+    for task in S.tasks():
+        task_name = task.name
+        task_is_optional = False  # it's not part of the Task class, but must be passed to the ort_solver
+        task_length = task.length
+        lower_interval_bound = 0
+        upper_interval_bound = S.horizon - task.length
+        interval_from_task = ort_solver.FixedDurationIntervalVar(lower_interval_bound,
+                                                                 upper_interval_bound,
+                                                                 task_length,
+                                                                 task_is_optional,
+                                                                 task_name)
+        task_to_interval[task] = interval_from_task
 
     # resource requirements
     for T in S.tasks():
@@ -52,70 +73,83 @@ def solve(scenario,time_limit=None,copy_scenario=False,msg=0) :
                 resource_to_intervals[R].append(I_)
                 RA_tasks.append(I_)
                 resource_task_to_interval[(R,T)] = I_
-                ort_solver.Add( I.StaysInSync(I_) )
+                ort_solver.Add(I.StaysInSync(I_))
                 # if resources are fixed
                 if T.resources is not None and R in T.resources :
-                    ort_solver.Add( I_.PerformedExpr() == 1 )
+                    ort_solver.Add(I_.PerformedExpr() == 1)
             # one resource needs to get selected
-            ort_solver.Add(ort_solver.Sum([ I_.PerformedExpr() for I_ in RA_tasks ]) == 1)
+            ort_solver.Add(ort_solver.Sum([I_.PerformedExpr() for I_ in RA_tasks]) == 1)
+
     ra_to_tasks = S.resources_req_tasks()
     for RA in ra_to_tasks:
         tasks = list(ra_to_tasks[RA])
         T = tasks[0]
-        for T_ in tasks[1:] :
-            for R in RA :
+        for T_ in tasks[1:]:
+            for R in RA:
                 I  = resource_task_to_interval[(R,T)]
                 I_ = resource_task_to_interval[(R,T_)]
-                ort_solver.Add( I.PerformedExpr() == I_.PerformedExpr() )
+                ort_solver.Add(I.PerformedExpr() == I_.PerformedExpr())
 
     # resources
     sequences = collections.OrderedDict()
     for R in S.resources() :
-        disj = ort_solver.DisjunctiveConstraint(resource_to_intervals[R],R.name)
+        disj = ort_solver.DisjunctiveConstraint(resource_to_intervals[R], R.name)
         sequences[R] = disj.SequenceVar()
         ort_solver.Add(disj)
 
     # move objective
     # TODO: bug, variables that are not part of the objective might not be finally defined
-    ort_objective_var = ort_solver.Sum([ task_to_interval[T].EndExpr()*T.delay_cost
+    ort_objective_var = ort_solver.Sum([task_to_interval[T].EndExpr()*T.delay_cost
                                          for T in S.tasks() if T in task_to_interval
-                                         and 'delay_cost' in T ])
+                                         and 'delay_cost' in T])
     ort_objective = ort_solver.Minimize(ort_objective_var, 1)
 
     # precedences lax
-    for P in S.precs_lax() :
-        ort_solver.Add( task_to_interval[P.task_right].StartsAfterEnd( task_to_interval[P.task_left] ) )
+    for P in S.precs_lax():
+        ort_solver.Add(task_to_interval[P.task_right].StartsAfterEnd(task_to_interval[P.task_left]))
         # TODO: add offset, but this requires DependecyGraph which is not exposed via swig?
 
     # precedences tight
-    for P in S.precs_tight() :
-        ort_solver.Add( task_to_interval[P.task_right].StartsAtEnd( task_to_interval[P.task_left] ) )
+    for P in S.precs_tight():
+        ort_solver.Add(task_to_interval[P.task_right].StartsAtEnd(task_to_interval[P.task_left]))
         # TODO: add offset, but this requires DependecyGraph which is not exposed via swig?
 
     # bound low
-    for P in S.bounds_low() :
-        ort_solver.Add( task_to_interval[P.task].StartsAfter(P.bound) )
+    for P in S.bounds_low():
+        ort_solver.Add(task_to_interval[P.task].StartsAfter(P.bound))
 
     # bound up
-    for P in S.bounds_up() :
-        ort_solver.Add( task_to_interval[P.task].StartsBefore(P.bound) )
+    for P in S.bounds_up():
+        ort_solver.Add(task_to_interval[P.task].StartsBefore(P.bound))
 
     # tight bound low
-    for P in S.bounds_low_tight() :
-        ort_solver.Add( task_to_interval[P.task].StartsAt(P.bound) )
+    for P in S.bounds_low_tight():
+        ort_solver.Add(task_to_interval[P.task].StartsAt(P.bound))
 
     # tight bound up
-    for P in S.bounds_up_tight() :
-        ort_solver.Add( task_to_interval[P.task].EndsAt(P.bound) )
+    for P in S.bounds_up_tight():
+        ort_solver.Add(task_to_interval[P.task].EndsAt(P.bound))
 
-    # capacity
+    # capacity TODO: this doesn't work
     for C in S.capacity():
-        # ignore sliced capacity constraints
-        if C._start is not None or C._end is not None:
-            continue
-        R = C.resource
-        cap_tasks = [ (resource_task_to_interval[R,T],C.weight(T=T,t=0)) for T in S.tasks() ]
-        ort_solver.Add( ort_solver.Sum([ I.PerformedExpr()*w for I,w in cap_tasks ]) >= C.bound )
+        for SL in C.slices_sum():
+            # ignore sliced capacity constraints
+            if SL._start is not None or SL._end is not None:
+               continue
+
+            R = SL.resource
+            
+            coeff = C.SLA[SL]
+
+            cap_tasks = []
+            for T in S.tasks():
+                w = coeff * SL.weight(T=T, t=0)
+                if not w.is_integer():  # might be coeff=w=1.0
+                    raise AssertionError("%s is not an integer." % w)
+                else:
+                    w = int(w)
+                cap_tasks.append((resource_task_to_interval[R,T], w))
+                ort_solver.Add(ort_solver.Sum([I.PerformedExpr() * w for I, w in cap_tasks]) >= C.bound)
 
     # creates search phases.
     vars_phase = ort_solver.Phase([ort_objective_var],
@@ -147,9 +181,9 @@ def solve(scenario,time_limit=None,copy_scenario=False,msg=0) :
     solutions_limit = int(1e8)
     limits = (ort_solver.Limit(ort_time_limit, branch_limit, failures_limit, solutions_limit, True))
 
-    # add log if mst is requested
+    # add log if msg is requested
     search_params = [limits,collector,ort_objective]
-    if msg :
+    if msg:
         search_params.append(search_log)
 
     # solves the problem.
@@ -164,8 +198,7 @@ def solve(scenario,time_limit=None,copy_scenario=False,msg=0) :
 
     # read last solution
     for T in S.tasks() :
-        T.start_value = int(solution.StartMin(task_to_interval[T])) #collector.StartValue(0, task_to_interval[T])
-        T.resources = [ R \
-                        for RA in T.resources_req for R in RA \
-                        if collector.PerformedValue(0,resource_task_to_interval[(R,T)]) == 1 ]
+        T.start_value = int(solution.StartMin(task_to_interval[T]))
+        T.resources = [R for RA in T.resources_req for R in RA \
+                       if collector.PerformedValue(0,resource_task_to_interval[(R,T)]) == 1]
     return True
